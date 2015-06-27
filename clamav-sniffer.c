ecd955517d0780a8b1d155da8aca152e002f248a
Nigel Horne <njh@bandsman.co.uk>
2015-06-27 17:51:09 +0100

    Increase the verbosity level needed for compare

--- clamav-sniffer.c
+++ clamav-sniffer.c
@@ -2422,14 +2422,12 @@ iswhitelisted(const union ip_addr *host_order_addr)
 	in_addr_t naddr = htonl(host_order_addr->i);
 
 	for(w = whitelist; w; w = w->next) {
-		if(verbose >= 3)
+		if(verbose >= 4)
 			fprintf(stderr, "Compare 0x%x (0x%x) 0x%x (0x%x) mask 0x%x\n",
 				htonl(w->addr.i), htonl(w->addr.i) & w->mask,
 				naddr, naddr & w->mask, w->mask);
-		if((htonl(w->addr.i) & w->mask) == (naddr & w->mask)) {
-			fprintf(stderr, "1\n");
+		if((htonl(w->addr.i) & w->mask) == (naddr & w->mask))
 			return 1;
-		}
 	}
 	return 0;
 }
